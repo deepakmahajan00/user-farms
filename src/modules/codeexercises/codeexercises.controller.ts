8c7784df5214bd12544f6f1f6942aa0dfad5257c
Deepak M <deepakmahajan00@gmail.com>
2023-11-08 12:03:11 +0100

    Refactor code

--- src/modules/codeexercises/codeexercises.controller.ts
+++ src/modules/codeexercises/codeexercises.controller.ts
@@ -2,16 +2,16 @@ import { NextFunction, Request, Response } from "express";
 
 export class CodeexercisesController {
 
-  public async transform(req: Request, res: Response, next: NextFunction) {
+  public transform(req: Request, res: Response, next: NextFunction) {
     try {
       console.log(req);
-      let data: any = ['super', '20.5', 'test', '23' ];
+      const data: any = ["super", "20.5", "test", "23"];
 
-      for (let key in data) {
+      for (const key in data) {
         // This line will transform values having number values
         // and for string type this will return NaN
         // i.e '20.5' & '23'
-        let newValue = parseFloat(data[key]);
+        const newValue = parseFloat(data[key]);
 
         // if Nan, i.e string, take it as it is and if not NaN take newly converted value.
         data[key] = isNaN(newValue) ? data[key] : newValue;
@@ -22,9 +22,9 @@ export class CodeexercisesController {
     }
   }
 
-  public async stringCheck(req: Request, res: Response, next: NextFunction) {
+  public stringCheck(req: Request, res: Response, next: NextFunction) {
     try {
-      let query = require('url').parse(req.url,true).query;
+      let query = require("url").parse(req.url,true).query;
       let result = false;
       const regex = /\d/;
       if (regex.test(query.inputString)) {
@@ -36,13 +36,13 @@ export class CodeexercisesController {
     }
   }
 
-  public async listFileNames(req: Request, res: Response, next: NextFunction) {
+  public listFileNames(req: Request, res: Response, next: NextFunction) {
     try {
-      const fs = require('fs');
-      let query = require('url').parse(req.url,true).query;
+      const fs = require("fs");
+      let query = require("url").parse(req.url,true).query;
       fs.access("./" + query.folderName, fs.constants.F_OK, (err: any) => {
         if (err) {
-            res.status(404).send('Folder not found');
+            res.status(404).send("Folder not found");
         } else {
             const files = this.getFilesInFolder(query.folderName, query.extension);
             res.status(200).send(files);
@@ -54,13 +54,12 @@ export class CodeexercisesController {
   }
 
   private getFilesInFolder(folderPath: string, extension: string): String[] {
-    const fs = require('fs');
-    const path = require('path');
+    const fs = require("fs");
+    const path = require("path");
     try {
         const files: string[] = fs.readdirSync("./" + folderPath);
-        console.log(files);
         const filteredFiles: string[] = files.filter((file: string) => {
-            return path.extname(file).toLowerCase() === '.' + extension.toLowerCase();
+            return path.extname(file).toLowerCase() === "." + extension.toLowerCase();
         });
         return filteredFiles;
     } catch (error) {
